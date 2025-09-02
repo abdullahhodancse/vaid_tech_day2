c5adbcff526b56352b3a09bccfbc1a08d988aa21
Abdullah Hodan <abdullahhodan448@gmail.com>
2025-09-02 18:23:59 +0600

    Describe your changes here

--- import.py
+++ import.py
@@ -1,4 +1,4 @@
-from list.py import * # i importted list.py file
+from listPractice import skills # i importted list.py file
 
 for skill in skills: # list.py e skills list er opor loop calano
   print (skill)
